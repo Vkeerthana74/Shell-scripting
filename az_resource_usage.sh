958c77ee410cc91c0ba3a4579169de4e4509375f
Keerthana <98146367+Vkeerthana74@users.noreply.github.com>
2025-11-30 14:32:34 +0530

    Add debugging and error handling to script

--- az_resource_usage.sh
+++ az_resource_usage.sh
@@ -6,14 +6,21 @@
 #version: v0.0.1
 #######################
 
+set -x
+set -e
+set -o
 # list blobs
+echo "list of Blobs"
 az storage blob list
 
 #list vms
+echo "list of vms"
 az vm list
 
 #list functions
+echo "list of Functions"
 az functionapp function list
 
 #list Entra users
+echo "list of Entra Users"
 az ad user list
